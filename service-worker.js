016bc2e6aa9242925d2bb32793f0a93c727f873b
Leonid Vegera <leonid.vegera@gmail.com>
2021-05-19 14:52:56 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.0d8f91351290a8ecf51da704f9aa35d3.js"
 );
 
 self.addEventListener('message', (event) => {
